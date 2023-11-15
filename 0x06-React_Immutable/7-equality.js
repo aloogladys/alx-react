985f94c213860f0bdb7bae57238e70776a3bf97d
Aloo <aloogladys@gmail.com>
2023-11-15 11:24:16 +0300

    fix

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,5 +1,5 @@
 import { is } from 'immutable';
 
-export default function areMapsEqual(map1, map2) {
-  return is(map1, map2);
+export default function areMapsEqual(page1, page2) {
+  return is(page1, page2);
 }
